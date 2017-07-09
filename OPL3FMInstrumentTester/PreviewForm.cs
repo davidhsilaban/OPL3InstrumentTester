e49d414a42e867e885e7f484024e901815dadd49
David Silaban <david.silaban@gmail.com>
2017-07-10 03:07:18 +0700

    Change audio library to NAudio

--- OPL3FMInstrumentTester/PreviewForm.cs
+++ OPL3FMInstrumentTester/PreviewForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,38 +15,102 @@ namespace OPL3FMInstrumentTester
 {
     public partial class PreviewForm : Form
     {
+        internal class SynthWaveOutProvider : NAudio.Wave.WaveProvider16
+        {
+            PreviewForm m_form;
+
+            public SynthWaveOutProvider(PreviewForm form)
+                : base(44100, 2)
+            {
+                m_form = form;
+            }
+
+            public override int Read(short[] buffer, int offset, int sampleCount)
+            {
+                //Debug.Print("ReadWave "+offset);
+                //short[] targetBuffer = buffer;
+                //if (m_form.audioBufferPos == m_form.audioBuffer.Length)
+                //{
+                //    for (int i = 0; i < sampleCount; i++)
+                //    {
+                //        targetBuffer[i] = m_form.audioBuffer[i];
+                //    }
+                //    return sampleCount;
+                //}
+                m_form.synth.getsample(buffer, sampleCount / Marshal.SizeOf(buffer[0].GetType()));
+
+                return sampleCount;
+            }
+        }
+
+        SynthWaveOutProvider naudioWaveProvider;
+        NAudio.Wave.WaveOut naudioWaveOut;
+
         WinMMWaveOut waveOut;
         FMSynth synth;
-        private Thread synthThread;
+        private Thread synthProcessingThread;
+        private Thread waveWriteThread;
         private bool isRendering;
         private object renderingLock = new object();
         private bool soundOn;
         private Stopwatch watch = new Stopwatch();
+        public short[] audioBuffer = new short[88200 * 2];
+        public int audioBufferPos = 0;
+        private uint[] op_offset = { 0, 1, 2, 8, 9, 10, 16, 17, 18 };
 
         public PreviewForm()
         {
             InitializeComponent();
 
             synth = new FMSynth();
-            waveOut = new WinMMWaveOut(2, 44100, 16);
-            waveOut.Open();
+            naudioWaveProvider = new SynthWaveOutProvider(this);
+            naudioWaveOut = new NAudio.Wave.WaveOut(this.Handle);
+            naudioWaveOut.NumberOfBuffers = 5;
+            //naudioWaveOut.DesiredLatency = 500;
+            naudioWaveOut.Init(naudioWaveProvider);
+            naudioWaveOut.Play();
+            //waveOut = new WinMMWaveOut(2, 44100, 16);
+            //waveOut.Open();
         }
 
-        private void SynthProcessingThread()
+        private void WaveWriteThreadEntry()
         {
             while (isRendering)
             {
-                short[] buffer = new short[4410*2];
-                Stopwatch stopwatch = new Stopwatch();
-                stopwatch.Start();
-                synth.getsample(buffer, 4410);
-                stopwatch.Stop();
-                //Debug.WriteLine("Render " + stopwatch.ElapsedMilliseconds);
-                stopwatch.Restart();
-                waveOut.Write(buffer);
-                stopwatch.Stop();
-                //Debug.WriteLine("Write " + stopwatch.ElapsedMilliseconds);
-                Thread.Sleep(1);
+                if (audioBufferPos == audioBuffer.Length)
+                {
+                    Debug.Print("Write");
+                    //waveOut.Write(audioBuffer);
+                    audioBufferPos = 0;
+                }
+            }
+        }
+
+        private void SynthProcessingThreadEntry()
+        {
+            while (isRendering)
+            {
+                lock (renderingLock)
+                {
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    short[] tempBuffer = new short[audioBuffer.Length / 10];
+                    while (audioBufferPos < audioBuffer.Length)
+                    {
+                        synth.getsample(tempBuffer, tempBuffer.Length);
+                        tempBuffer.CopyTo(audioBuffer, audioBufferPos);
+                        audioBufferPos += tempBuffer.Length;
+                    }
+                    stopwatch.Stop();
+                    //Debug.WriteLine("Render " + stopwatch.ElapsedMilliseconds);
+                    stopwatch.Restart();
+                    //waveOut.Write(audioBuffer);
+                    stopwatch.Stop();
+                    //audioBufferPos = 0;
+                    //Debug.WriteLine("Write " + stopwatch.ElapsedMilliseconds);
+                    //Monitor.Wait(renderingLock, 10);
+                    //Thread.Sleep(2000);
+                }
             }
         }
 
@@ -83,22 +148,25 @@ namespace OPL3FMInstrumentTester
 
             /*Debug.WriteLine(trem_vibr_sust_ksr_mult_op1);*/
 
-            // Write OP 1 to synthesizer
-            synth.write(0x20, (byte)trem_vibr_sust_ksr_mult_op1);
-            synth.write(0x40, (byte)ksl_tl_op1);
-            synth.write(0x60, (byte)ar_dr_op1);
-            synth.write(0x80, (byte)sr_rr_op1);
-            synth.write(0xE0, (byte)waveform_op1);
+            for (int c = 0; c < 9; c++)
+            {
+                // Write OP 1 to synthesizer
+                synth.write(0x20+op_offset[c], (byte)trem_vibr_sust_ksr_mult_op1);
+                synth.write(0x40 + op_offset[c], (byte)ksl_tl_op1);
+                synth.write(0x60 + op_offset[c], (byte)ar_dr_op1);
+                synth.write(0x80 + op_offset[c], (byte)sr_rr_op1);
+                synth.write(0xE0 + op_offset[c], (byte)waveform_op1);
 
-            // Write OP 2 to synthesizer
-            synth.write(0x23, (byte)trem_vibr_sust_ksr_mult_op2);
-            synth.write(0x43, (byte)ksl_tl_op2);
-            synth.write(0x63, (byte)ar_dr_op2);
-            synth.write(0x83, (byte)sr_rr_op2);
-            synth.write(0xE3, (byte)waveform_op2);
+                // Write OP 2 to synthesizer
+                synth.write(0x23 + op_offset[c], (byte)trem_vibr_sust_ksr_mult_op2);
+                synth.write(0x43 + op_offset[c], (byte)ksl_tl_op2);
+                synth.write(0x63 + op_offset[c], (byte)ar_dr_op2);
+                synth.write(0x83 + op_offset[c], (byte)sr_rr_op2);
+                synth.write(0xE3 + op_offset[c], (byte)waveform_op2);
 
-            // Write CH 1 to synthesizer
-            synth.write(0xC0, (byte)fb_alg);
+                // Write CH 1 to synthesizer
+                synth.write(0xC0+(uint)c, (byte)fb_alg);
+            }
         }
 
         private void SoundOnSingleNote()
@@ -113,6 +181,36 @@ namespace OPL3FMInstrumentTester
             synth.write(0xB0, (4 << 2) | (0x01));
         }
 
+        private void SoundOnCMajorChord()
+        {
+            // C4
+            synth.write(0xA0, 0x5A);
+            synth.write(0xB0, (1 << 5) | (4 << 2) | (0x01));
+
+            // E4
+            synth.write(0xA1, 0xB3);
+            synth.write(0xB1, (1 << 5) | (4 << 2) | (0x01));
+
+            // G4
+            synth.write(0xA2, 0x04);
+            synth.write(0xB2, (1 << 5) | (4 << 2) | (0x02));
+        }
+
+        private void SoundOffCMajorChord()
+        {
+            // C4
+            synth.write(0xA0, 0x5A);
+            synth.write(0xB0, (0 << 5) | (4 << 2) | (0x01));
+
+            // E4
+            synth.write(0xA1, 0xB3);
+            synth.write(0xB1, (0 << 5) | (4 << 2) | (0x01));
+
+            // G4
+            synth.write(0xA2, 0x04);
+            synth.write(0xB2, (0 << 5) | (4 << 2) | (0x02));
+        }
+
         private void PreviewForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             lock (renderingLock)
@@ -121,7 +219,8 @@ namespace OPL3FMInstrumentTester
 
                 isRendering = false;
                 Monitor.Pulse(renderingLock);
-                waveOut.Close();
+                //waveOut.Close();
+                naudioWaveOut.Dispose();
             }
         }
 
@@ -133,8 +232,11 @@ namespace OPL3FMInstrumentTester
                 Monitor.Pulse(renderingLock);
             }
 
-            synthThread = new Thread(SynthProcessingThread);
-            synthThread.Start();
+            //synthProcessingThread = new Thread(SynthProcessingThreadEntry);
+            //synthProcessingThread.Start();
+
+            //waveWriteThread = new Thread(WaveWriteThreadEntry);
+            //waveWriteThread.Start();
 
             timer1.Enabled = true;
         }
@@ -182,6 +284,33 @@ namespace OPL3FMInstrumentTester
                 SoundOffSingleNote();
                 watch.Stop();
                 Debug.WriteLine("Off: " + watch.ElapsedMilliseconds);
+            }
+        }
+
+        private void buttonCMajorChord_MouseDown(object sender, MouseEventArgs e)
+        {
+            SetupFMParameters();
+            SoundOnCMajorChord();
+        }
+
+        private void buttonCMajorChord_MouseUp(object sender, MouseEventArgs e)
+        {
+            SoundOffCMajorChord();
+        }
+
+        private void buttonCMajorChord_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                buttonCMajorChord_MouseDown(sender, null);
+            }
+        }
+
+        private void buttonCMajorChord_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                buttonCMajorChord_MouseUp(sender, null);
             }
         }
     }
