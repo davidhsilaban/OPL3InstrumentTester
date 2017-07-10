dd1c0078e881dce3d86bb717032b1fd01b925e56
David Silaban <david@infotech.co.id>
2017-07-10 18:35:43 +0700

    IBK reader

---
+++ OPL3FMInstrumentTester/IBKHeader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPL3FMInstrumentTester
+{
+    [StructLayout(LayoutKind.Explicit)]
+    class IBKHeader
+    {
+        [FieldOffset(0)]
+        public Byte[] Signature = new Byte[]{ (byte)'I', (byte)'B', (byte)'K', 0x1A };
+        [FieldOffset(4)]
+        public IBKInstrument[] InstrumentData; // 128 entries
+        [FieldOffset(2052)]
+        public String[] Names;
+    }
+}
---
+++ OPL3FMInstrumentTester/IBKInstrument.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPL3FMInstrumentTester
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 16)]
+    class IBKInstrument
+    {
+        public Byte iModChar;
+        public Byte iCarChar;
+        public Byte iModScale;
+        public Byte iCarScale;
+        public Byte iModAttack;
+        public Byte iCarAttack;
+        public Byte iModSustain;
+        public Byte iCarSustain;
+        public Byte iModWaveSel;
+        public Byte iCarWaveSel;
+        public Byte iFeedback;
+        //Byte[] Padding;
+    }
+}
